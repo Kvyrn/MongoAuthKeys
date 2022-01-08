a2d7cf8cd5532e95283a839caa928a75a9bae2bb
Treesoid <70215358+Treesoid@users.noreply.github.com>
2022-01-08 09:39:50 +0100

    initial

---
+++ src/main/java/io/github/treesoid/mongoauthkeys/MongoAuthKeys.java
@@ -0,0 +1,12 @@
+package io.github.treesoid.mongoauthkeys;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MongoAuthKeys implements ModInitializer {
+    public static final String modid = "mongo-auth-keys";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
